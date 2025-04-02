17ae8db61a011fd69c9295cfa2a46ff326e84620
小沐沐吖 <sunnyboy_mu@163.com>
2025-04-02 22:12:17 +0800

    styles: fix message and message-box style

--- lx-ui/src/as-needed.js
+++ lx-ui/src/as-needed.js
@@ -2,6 +2,9 @@
 // 引入 ProForm 的 div 的 tag 组件样式
 import 'element-plus/es/components/collapse/style/index';
 import 'element-plus/es/components/collapse-item/style/index';
+import 'element-plus/es/components/message-box/style/index';
+import 'element-plus/es/components/message/style/index';
+import 'element-plus/es/components/loading/style/index';
 
 const installer = {
   install(_app) {}
