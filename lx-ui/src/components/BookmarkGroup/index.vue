bf51a4be6e8266cf47f4494f8622e646cdab001b
小沐沐吖 <sunnyboy_mu@163.com>
2025-04-17 10:01:29 +0800

    fix: fix styles

--- lx-ui/src/components/BookmarkGroup/index.vue
+++ lx-ui/src/components/BookmarkGroup/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white p-4 rounded-md">
+  <div class="bg-white p-4 rounded-md" anchor-node>
     <div class="flex items-center justify-between">
       <div class="text-sm">
         <i class="iconfont icon-shuqian"></i>
